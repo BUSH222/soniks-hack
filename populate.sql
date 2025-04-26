269e45bbabe4dc997962a0e5caf27b555ff78de0
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2025-04-27 00:48:54 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,2 +1,7 @@
 insert into "user" (name,password) values
 ('Bob','123');
+
+insert into "station"  (name,lat,long,alt) values
+('NIGGER',55.7522,37.6156,123);
+insert into ownership (user_id,station_id) values
+(1,1);
