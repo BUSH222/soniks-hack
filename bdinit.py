5756c9ddcb63e80fc774ff721cd163df9c3aaea2
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2025-04-27 15:26:28 +0300

    Update bdinit.py

--- bdinit.py
+++ bdinit.py
@@ -7,13 +7,20 @@ def init_bd():
     Base.metadata.create_all(bind=engine)
 
 def populate_base_data():
-    db_session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
+    db_session = Session()
     bob = User(name = "Bob",password = '123')
     bob_station = Station(name='NIGGER',lat=55.7522,long=37.6156,alt=123)
-    bob_own = Ownership(user_id=1,station_id=1)
-    db_session.add_all([bob,bob_own,bob_station])
+    db_session.add_all([bob,bob_station])
     try:
         db_session.commit()
     except Exception as e:
         raise e
-  
+    user_id = db_session.query(User).first().id
+    station_id = db_session.query(Station).first().id
+    bob_own = Ownership(user_id=user_id,station_id=station_id)
+    db_session.add_all([bob_own])
+    try:
+        db_session.commit()
+    except Exception as e:
+        raise e
