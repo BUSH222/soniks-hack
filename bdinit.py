1269c3ac2005d0aa841638566e410222a55fa6fe
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2025-04-27 16:59:02 +0300

    Update bdinit.py

--- bdinit.py
+++ bdinit.py
@@ -17,7 +17,7 @@ def populate_base_data():
     except Exception as e:
         raise e
     user_id = db_session.query(User).first().id
-    station_id = db_session.query(Station).all().[0].id
+    station_id = db_session.query(Station).all()[0].id
     bob_own = Ownership(user_id=user_id,station_id=station_id)
     db_session.add_all([bob_own])
     try:
